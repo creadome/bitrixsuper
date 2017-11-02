3375a6cb3ba22c53f9f7d69f08c053c337e63b29
Stanislav Vasilyev <creadome@gmail.com>
2017-11-02 20:01:23 +0800

    Оптимизация
    Отключено кеширование ключей

--- super/component.php
+++ super/component.php
@@ -1,4 +1,8 @@
 <?
 	if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-	if ($this->StartResultCache()) $this->IncludeComponentTemplate();
+
+	if ($this->StartResultCache()) {
+		$this->SetResultCacheKeys(array());
+		$this->IncludeComponentTemplate();
+	}
 ?>
